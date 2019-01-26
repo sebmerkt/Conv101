c31fb36d526c48fc96bafde0a0256ba8742030e6
sebmerkt <sebastian.merkt@gmail.com>
2019-01-26 13:25:20 -0500

    Remodel of main activity layout

--- everydayconverterwear/src/main/java/io/github/sebmerkt/everydayconverterwear/MainActivityWear.java
+++ everydayconverterwear/src/main/java/io/github/sebmerkt/everydayconverterwear/MainActivityWear.java
@@ -19,11 +19,15 @@ package io.github.sebmerkt.everydayconverterwear;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.wear.widget.WearableLinearLayoutManager;
+import android.support.wear.widget.WearableRecyclerView;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.wearable.intent.RemoteIntent;
+
+import java.util.ArrayList;
 
 public class MainActivityWear extends WearableActivity {
 
@@ -31,97 +35,64 @@ public class MainActivityWear extends WearableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_wear);
+        WearableRecyclerView recyclerView = findViewById(R.id.main_menu_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setEdgeItemsCenteringEnabled(true);
+        recyclerView.setLayoutManager(new WearableLinearLayoutManager(this));
+
+        ArrayList<MainMenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MainMenuItem(R.drawable.ic_length_ruler, getResources().getString(R.string.string_length)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_area, getResources().getString(R.string.string_area)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_volume, getResources().getString(R.string.string_volume)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_clock, getResources().getString(R.string.string_time)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_speed, getResources().getString(R.string.string_speed)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_temp, getResources().getString(R.string.string_temperature)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_weight, getResources().getString(R.string.string_weight)));
+        menuItems.add(new MainMenuItem(R.drawable.ic_storage, getResources().getString(R.string.string_storage)));
+
+        recyclerView.setAdapter(new MainMenuItems(this, menuItems, new MainMenuItems.AdapterCallback() {
+            @Override
+            public void onItemClicked(final Integer menuPosition) {
+                switch (menuPosition){
+                    case 0:
+                        Intent mLengthIntent = new Intent(MainActivityWear.this, ConvertUnitsLength.class);
+                        MainActivityWear.this.startActivity(mLengthIntent);
+                        break;
+                    case 1:
+                        Intent mAreaIntent = new Intent(MainActivityWear.this, ConvertUnitsArea.class);
+                        MainActivityWear.this.startActivity(mAreaIntent);
+                        break;
+                    case 2:
+                        Intent mVolumeIntent = new Intent(MainActivityWear.this, ConvertUnitsVolume.class);
+                        MainActivityWear.this.startActivity(mVolumeIntent);
+                        break;
+                    case 3:
+                        Intent mTimeIntent = new Intent(MainActivityWear.this, ConvertUnitsTime.class);
+                        MainActivityWear.this.startActivity(mTimeIntent);
+                        break;
+                    case 4:
+                        Intent mSpeedIntent = new Intent(MainActivityWear.this, ConvertUnitsSpeed.class);
+                        MainActivityWear.this.startActivity(mSpeedIntent);
+                        break;
+                    case 5:
+                        Intent mTempIntent = new Intent(MainActivityWear.this, ConvertUnitsTemp.class);
+                        MainActivityWear.this.startActivity(mTempIntent);
+                        break;
+                    case 6:
+                        Intent mWeigthIntent = new Intent(MainActivityWear.this, ConvertUnitsWeight.class);
+                        MainActivityWear.this.startActivity(mWeigthIntent);
+                        break;
+                    case 7:
+                        Intent mStorageIntent = new Intent(MainActivityWear.this, ConvertUnitsStorage.class);
+                        MainActivityWear.this.startActivity(mStorageIntent);
+                        break;
+                }
+            }
+        }));
 
         // Enables Always-on
         setAmbientEnabled();
-
-        Button lengthButton = findViewById(R.id.button_length);
-        lengthButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mLengthIntent = new Intent(MainActivityWear.this, ConvertUnitsLength.class);
-                MainActivityWear.this.startActivity(mLengthIntent);
-            }
-        });
-
-        Button areaButton = findViewById(R.id.button_area);
-        areaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mAreaIntent = new Intent(MainActivityWear.this, ConvertUnitsArea.class);
-                MainActivityWear.this.startActivity(mAreaIntent);
-            }
-        });
-
-        Button speedButton = findViewById(R.id.button_speed);
-        speedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mSpeedIntent = new Intent(MainActivityWear.this, ConvertUnitsSpeed.class);
-                MainActivityWear.this.startActivity(mSpeedIntent);
-            }
-        });
-
-        Button storageButton = findViewById(R.id.button_storage);
-        storageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mStorageIntent = new Intent(MainActivityWear.this, ConvertUnitsStorage.class);
-                MainActivityWear.this.startActivity(mStorageIntent);
-            }
-        });
-
-        Button tempButton = findViewById(R.id.button_temperature);
-        tempButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mTempIntent = new Intent(MainActivityWear.this, ConvertUnitsTemp.class);
-                MainActivityWear.this.startActivity(mTempIntent);
-            }
-        });
-
-        Button volumeButton = findViewById(R.id.button_volume);
-        volumeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mVolumeIntent = new Intent(MainActivityWear.this, ConvertUnitsVolume.class);
-                MainActivityWear.this.startActivity(mVolumeIntent);
-            }
-        });
-
-        Button timeButton = findViewById(R.id.button_time);
-        timeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mTimeIntent = new Intent(MainActivityWear.this, ConvertUnitsTime.class);
-                MainActivityWear.this.startActivity(mTimeIntent);
-            }
-        });
-
-        Button weightButton = findViewById(R.id.button_weight);
-        weightButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mWeightIntent = new Intent(MainActivityWear.this, ConvertUnitsWeight.class);
-                MainActivityWear.this.startActivity(mWeightIntent);
-            }
-        });
-
-        Button infoButton = findViewById(R.id.button_info);
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mInfoIntent = new Intent(Intent.ACTION_VIEW)
-                        .addCategory(Intent.CATEGORY_BROWSABLE)
-                        .setData(Uri.parse(getString(R.string.string_blog)));
-
-                RemoteIntent.startRemoteActivity(MainActivityWear.this, mInfoIntent, null);
-
-            }
-        });
+//
 
     }
 }
-
-
-//TODO: Improvement: Button for "Open app on phone" for unit explanation etc.
---
+++ everydayconverterwear/src/main/java/io/github/sebmerkt/everydayconverterwear/MainMenuItems.java
@@ -0,0 +1,90 @@
+package io.github.sebmerkt.everydayconverterwear;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainMenuItems extends RecyclerView.Adapter<MainMenuItems.RecyclerViewHolder> {
+
+    private ArrayList<MainMenuItem> dataSource = new ArrayList<MainMenuItem>();
+    public interface AdapterCallback{
+        void onItemClicked(Integer menuPosition);
+    }
+    private AdapterCallback callback;
+
+    public MainMenuItems(Context context, ArrayList<MainMenuItem> dataArgs, AdapterCallback callback){
+        Context context1 = context;
+        this.dataSource = dataArgs;
+        this.callback = callback;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_menu_item,parent,false);
+
+        RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
+
+        return recyclerViewHolder;
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder
+    {
+        RelativeLayout menuContainer;
+        TextView menuItem;
+        ImageView menuIcon;
+
+        public RecyclerViewHolder(View view) {
+            super(view);
+            menuContainer = view.findViewById(R.id.menu_container);
+            menuItem = view.findViewById(R.id.menu_item);
+            menuIcon = view.findViewById(R.id.menu_icon);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, final int position) {
+        MainMenuItem data_provider = dataSource.get(position);
+
+        holder.menuItem.setText(data_provider.getText());
+        holder.menuIcon.setImageResource(data_provider.getImage());
+        holder.menuContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+                if(callback != null) {
+                    callback.onItemClicked(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.size();
+    }
+}
+
+class MainMenuItem {
+    private String text;
+    private int image;
+
+    public MainMenuItem(int image, String text) {
+        this.image = image;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
